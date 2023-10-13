8ebf91a4497c9478cff05f6b056ed81e801c95d4
chloeblyau <chloe.blyau@eleve.ensai.fr>
2023-10-13 14:37:18 +0200

    Update main.py
    api

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-import numpy as np
+from fastapi import FastAPI
+
+
+# On instancie le webservice
+app = FastAPI()
+
+# Création d'un enpoint qui répond à la méthode GET à l'adresse "/" qui va retourne le message "Hello World"
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+# Lancement de l'application sur le le port 80
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=80)
