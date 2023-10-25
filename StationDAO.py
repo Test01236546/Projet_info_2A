d4f61d2fded26bbd229c7949a536bdf549e69905
jeremie <129837718+Test01236546@users.noreply.github.com>
2023-10-25 16:04:39 +0200

    maj stationDAO

--- StationDAO.py
+++ StationDAO.py
@@ -1,5 +1,37 @@
-from AbstractDAO import DAO
+#il faut modifier les listes (instructions python)
+#modifier injest
 
-class StationDAO (DAO):
+class StationDAO:
     def __init__(self):
-        pass
+        self.stations = []
+
+    def create(self, station):
+        self.stations.append(station)
+        print(f"Station créée : {station}")
+
+    def read(self, id):
+        for station in self.stations:
+            if station.id == id:
+                print(f"Station trouvée : {station}")
+                return station
+        print("Station non trouvée")
+        return None
+
+    def update(self, id, new_station):
+        for i, station in enumerate(self.stations):
+            if station.id == id:
+                self.stations[i] = new_station
+                print(f"Station mise à jour : {new_station}")
+                return
+        print("Station non trouvée")
+
+    def delete(self, id):
+        for i, station in enumerate(self.stations):
+            if station.id == id:
+                del self.stations[i]
+                print(f"Station supprimée : {station}")
+                return
+        print("Station non trouvée")
+
+    def insert(self, station):
+        self.create(station)
