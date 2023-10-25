9f20f2d7812e525516641c779843c5f0fe039f08
chloeblyau <chloe.blyau@eleve.ensai.fr>
2023-10-25 10:53:13 +0200

    ajout classes +changements

---
+++ Service.py
@@ -0,0 +1,12 @@
+class Service ():
+    def __init__(self,id_stationfaits, id_station, id_temps):
+        self.id_stationfaits=id_stationfaits
+        self.id_station=id_station
+        self.id_temps=id_temps
+
+    def F1(self, position):
+        pass
+    def F1(self, periode):
+        pass
+    def F1(self, periode):
+        pass
--- main.py
+++ main.py
@@ -1,14 +1,34 @@
-from fastapi import FastAPI
+import fastapi
+import requests
+from pydantic import BaseModel
+from Station import Station
 
-# On instancie le webservice
-app = FastAPI()
-
-# Création d'un enpoint qui répond à la méthode GET à l'adresse "/" qui va retourne le message "Hello World"
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-# Lancement de l'application sur le le port 80
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=80)
+def get_coordinates(address):
+    url = "https://api.etalab.gouv.fr/geoportail/adresses/v2/?q={}".format(address)
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data["features"][0]["geometry"]["coordinates"]
+    else:
+        return None
     
+
+app = fastapi.FastAPI()
+
+@app.get("/closest_station/")
+async def get_closest_station(address: Address):
+    coordinates = get_coordinates(address.formatted_address)
+    if coordinates is not None:
+        return get_closest_station(coordinates)
+    else:
+        return "Aucune station trouvée"
+
+@app.get("/least_frequented_station/")
+async def get_least_frequented_station(start_date: str, end_date: str):
+    return get_least_frequented_station(start_date, end_date)
+
+@app.get("/most_frequented_district/")
+async def get_most_frequented_district(start_date: str, end_date: str):
+    return get_most_frequented_district(start_date, end_date)
+
+app.run(debug=True)
