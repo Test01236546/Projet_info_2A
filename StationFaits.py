89209a9b555d8b631ba3f210b6826fe86780edc6
chloeblyau <chloe.blyau@eleve.ensai.fr>
2023-10-25 10:13:26 +0200

    Create StationFaits.py
    creation init

---
+++ StationFaits.py
@@ -0,0 +1,8 @@
+class StationFaits():
+    def __init__(self, id, id_temps, velos_dispos, meca_dispo, elec_dispo, retour_velo ):
+        self.id=id
+        self.id_temps=id_temps
+        self.velos_dispos=velos_dispos
+        self.meca_dispo=meca_dispo
+        self.elec_dispo=elec_dispo
+        self.retour_velo=retour_velo
