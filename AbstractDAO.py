a66cd0a880d9b4a58867d75c7099909f57de6bfb
jeremie <129837718+Test01236546@users.noreply.github.com>
2023-10-26 14:58:05 +0200

    maj

--- AbstractDAO.py
+++ AbstractDAO.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 
-# il faudait faire raise error si les class fille n'ont pas les méthodes
+# il faudait faire raise error si les class fille n'ont pas les méthodes rt
 
 class StationDAO(ABC):
     @abstractmethod
