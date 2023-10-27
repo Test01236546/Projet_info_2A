aeca4ae9707fb410c386039b6eda55e10a2e3314
KarlKamp <charles.fougeroux-de-campigneulles@eleve.ensai.fr>
2023-10-27 14:53:49 +0200

    modif prise en compte etalab

--- F1.py
+++ F1.py
@@ -19,37 +19,41 @@ def get_coordinates_from_address(address):
         return None, None
 
 def trouver_station_proche(lat, lon):
-    # Faire une requête à l'API
-    null=None # car dans la base de donnée response 
-    url = "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/velib-disponibilite-en-temps-reel/exports/json?lang=fr&timezone=Europe%2FBerlin"
-    response = requests.get(url)
+    
+    if lat is not None and lon is not None:
+        # Faire une requête à l'API
+        null=None # car dans la base de donnée response 
+        url = "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/velib-disponibilite-en-temps-reel/exports/json?lang=fr&timezone=Europe%2FBerlin"
+        response = requests.get(url)
     
 
-    if response.status_code == 200:
-        stations_data = response.json()
+        if response.status_code == 200:
+            stations_data = response.json()
         
         
-        # Filtre les stations qui sont installées et ouvertes à la location et qui ont au moins 1 vélo dispo.
-        stations_utilisables = [station for station in stations_data if station["is_installed"] == "OUI" and station["is_renting"] == "OUI" and station["numbikesavailable"] >= 1]
+            # Filtre les stations qui sont installées et ouvertes à la location et qui ont au moins 1 vélo dispo.
+            stations_utilisables = [station for station in stations_data if station["is_installed"] == "OUI" and station["is_renting"] == "OUI" and station["numbikesavailable"] >= 1]
 
-        #verification qu'il y au moins une station utilisable
-        if stations_utilisables:
+            #verification qu'il y au moins une station utilisable
+            if stations_utilisables:
             
-            # Calcule la distance entre la position et chaque station.
-            distances = []
-            for station in stations_utilisables:
-                distance = geopy.distance.distance((lon, lat), (station["coordonnees_geo"]["lon"],station["coordonnees_geo"]["lat"])).km
-                distances.append((distance, station["name"]))
+                # Calcule la distance entre la position et chaque station.
+                distances = []
+                for station in stations_utilisables:
+                    distance = geopy.distance.distance((lon, lat), (station["coordonnees_geo"]["lon"],station["coordonnees_geo"]["lat"])).km
+                    distances.append((distance, station["name"]))
 #station["name"] a remplacer à la place de station
             # Renvoie la station avec la distance la plus courte.
 
-            return min(distances, key=lambda x: x[0])[1]
+                return min(distances, key=lambda x: x[0])[1]
     
-        else:
-            return "Aucune station avec un vélo disponible à proximité."
+            else:
+                return "Aucune station avec un vélo disponible à proximité."
         
-    else:
-        return "Erreur lors de la requête à l'API."
+        else:
+            return "Erreur lors de la requête à l'API."
+    else :
+        print("Impossible de trouver les coordonnées de l'adresse.")
 
 # Coordonnées à partir desquelles vous souhaitez trouver la station
 latitude = 48.8182338426597
@@ -60,10 +64,12 @@ print(f"La station la plus proche est : {nom_station_proche}")
 
 # Utilisation des fonctions
 address = "30, rue de Sèvres, 75007"
-lat, lon = get_coordinates_from_address(address)
+station = trouver_station_proche(get_coordinates_from_address(address))
+print(f"La station Vélib la plus proche de l'adresse est : {station}")
+#lat, lon = get_coordinates_from_address(address)
 
-if lat is not None and lon is not None:
-    station = trouver_station_proche(lat, lon)
-    print(f"La station Vélib la plus proche de l'adresse est : {station}")
-else:
-    print("Impossible de trouver les coordonnées de l'adresse.")
+#if lat is not None and lon is not None:
+#    station = trouver_station_proche(lat, lon)
+#    print(f"La station Vélib la plus proche de l'adresse est : {station}")
+#else:
+#    print("Impossible de trouver les coordonnées de l'adresse.")
--- Fonctionnalites.py
+++ Fonctionnalites.py
@@ -7,38 +7,56 @@ class Fonctionnalites():
         pass
     
     
-    def F1(self,lat,lon):
-       # Faire une requête à l'API
-        null=None # car dans la base de donnée response 
-        url = "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/velib-disponibilite-en-temps-reel/exports/json?lang=fr&timezone=Europe%2FBerlin"
-        response = r.get(url)
-    
+    def F1(self,address):
+        def get_coordinates_from_address(address):
+            # Faire une requête à l'API Etalab
+            url = f"https://api-adresse.data.gouv.fr/search/?q={address}&limit=1"
+            response = requests.get(url)
 
-        if response.status_code == 200:
-            stations_data = response.json()
-        
-        
-        # Filtre les stations qui sont installées et ouvertes à la location et qui ont au moins 1 vélo dispo.
-            stations_utilisables = [station for station in stations_data if station["is_installed"] == "OUI" and station["is_renting"] == "OUI" and station["numbikesavailable"] >= 1]
+            if response.status_code == 200:
+                data = response.json()
+                if data['features']:
+                    lon, lat = data['features'][0]['geometry']['coordinates']
+                    return lat, lon
+                else:
+                    return None, None
+            else:
+                return None, None
 
-        #verification qu'il y au moins une station utilisable
-            if stations_utilisables:
+        lat, lon = get_coordinates_from_address(address) #on obtient les coordonnées gps à partir de l'adresse
+
+        if lat is not None and lon is not None:
+            # Faire une requête à l'API
+            null = None
+            url = "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/velib-disponibilite-en-temps-reel/exports/json?lang=fr&timezone=Europe%2FBerlin"
+            response = requests.get(url)
+
+            if response.status_code == 200:
+                stations_data = response.json()
+        
+                # Filtre les stations qui sont installées et ouvertes à la location et qui ont au moins 1 vélo dispo.
+                stations_utilisables = [station for station in stations_data if station["is_installed"] == "OUI" and station["is_renting"] == "OUI" and station["numbikesavailable"] >= 1]
+
+                # Vérification qu'il y a au moins une station utilisable
+                if stations_utilisables:
             
-            # Calcule la distance entre la position et chaque station.
-                distances = []
-                for station in stations_utilisables:
-                    distance = geopy.distance.distance((lon, lat), (station["coordonnees_geo"]["lon"],station["coordonnees_geo"]["lat"])).km
-                    distances.append((distance, station["name"]))
+                    # Calcule la distance entre la position et chaque station.
+                    distances = []
+                    for station in stations_utilisables:
+                        distance = geopy.distance.distance((lon, lat), (station["coordonnees_geo"]["lon"],station["coordonnees_geo"]["lat"])).km
+                        distances.append((distance, station["name"]))
 
-            # Renvoie la station avec la distance la plus courte.
-
-                return min(distances, key=lambda x: x[0])[1]
+                    # Renvoie le nom de la station avec la distance la plus courte.
+                    return min(distances, key=lambda x: x[0])[1]
+    
+                else:
+                    return "Aucune station avec un vélo disponible à proximité."
     
             else:
-                return "Aucune station avec un vélo disponible à proximité."
-        
+                return "Erreur lors de la requête à l'API."
         else:
-            return "Erreur lors de la requête à l'API."
+            return "Impossible de trouver les coordonnées de l'adresse."
+
 
     def F2(self):
         pass
