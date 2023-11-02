e9356c98c46e0a01f0971ba2880e4c751f579ac7
jeremie <129837718+Test01236546@users.noreply.github.com>
2023-11-02 11:36:50 +0100

    maj

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from API import StationAPI, app 
 from fastapi import FastAPI
-
+from BDD.constantes import BDD_PATH
 
 import requests as r 
 from pydantic import BaseModel
-from Service.Station import Station
+from Service.station import Station
 import uvicorn
-from Service.Service import Service
+from Service.service import Service
 from datetime import datetime
-import Service.Fonctionnalites as F
+import Service.fonctionnalites as F
 from geopy.geocoders import Nominatim 
-from BDD.classBDD import BDD_Manager, BDD_PATH
-from DAO.StationDAO import StationDAO
+from BDD.classBDD import BDD_Manager
+from DAO.stationDAO import StationDAO
+
 if __name__ == "__main__":
     while True:
         BDD_Manager(BDD_PATH).create_stations_table()
