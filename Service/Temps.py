9af3e79bea17e311738a31f4be7ca78bed0e9775
chloeblyau <chloe.blyau@eleve.ensai.fr>
2023-11-08 15:10:26 +0100

    Update Temps.py

--- Service/Temps.py
+++ Service/Temps.py
@@ -1,6 +1,9 @@
 
 
 class Temps:
+    """
+    Crée la classe Temps
+    """
     def __init__(self, id_temps, date, annee, mois, jour, heure, minute):
         self.id_temps = id_temps
         self.date = date
@@ -11,4 +14,10 @@ class Temps:
         self.minute = minute
 
     def __repr__(self):
+        """
+        Renvoie une représentation sous forme de chaîne de caractères de l'objet Temps.
+
+        Returns:
+            str: Une chaîne de caractères représentant l'objet Temps.
+        """
         return (f"Temps(id_temps={self.id_temps}, date={self.date}, annee={self.annee}, mois={self.mois},jour={self.jour}, heure={self.heure}, minute={self.minute})")
