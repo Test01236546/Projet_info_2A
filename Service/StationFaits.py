3a002ac4ac1ed6e698a97e4619bd6eb3961d5131
chloeblyau <chloe.blyau@eleve.ensai.fr>
2023-11-08 15:09:15 +0100

    Update StationFaits.py

--- Service/StationFaits.py
+++ Service/StationFaits.py
@@ -1,5 +1,8 @@
 
 class StationFaits():
+    """"
+    Crée la classe StationFaits
+    """
     def __init__(self, id_station, nb_bornettes, velos_dispos, meca_dispo, elec_dispo, retour_velo, frequence, date_fait_deb, date_fait_fin ):
         self.id_station=id_station
         self.nb_bornettes=nb_bornettes
@@ -12,4 +15,10 @@ class StationFaits():
         self.date_fait_fin=date_fait_fin
 
     def __str__(self):
+        """
+        Renvoie une représentation sous forme de chaîne de caractères de l'objet StationFaits.
+
+        Returns:
+            str: Une chaîne de caractères représentant l'objet StationFaits.
+        """
         return (f"StationFaits(id={self.id_station}, nb_bornettes={self.nb_bornettes}, velos_dispos={self.velos_dispos},meca_dispo={self.meca_dispo}, elec_dispo={self.elec_dispo}, retour_velo={self.retour_velo}, frequence={self.frequence}, date_fait_deb={self.date_fait_deb}, date_fait_fin={self.date_fait_fin})")
